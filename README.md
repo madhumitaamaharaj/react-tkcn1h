3c6aea062b8d664647b349d4c83c196f8e2c2d96
madhumitaamaharaj <106608359+madhumitaamaharaj@users.noreply.github.com>
2023-03-24 23:12:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-tkcn1h
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-tkcn1h)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+import Board from "./components/Board";
+
+function App() {
+  return (
+    <div className="App">
+      <Board />
+      <tic/>
+    </div>
+  );
+}
+
+export default App;
